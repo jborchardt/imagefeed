862d695a7afc88d567db426b048c1b3c4316a8eb
Johannes Borchardt <borchardt@andlabs.com>
2017-03-23 13:55:35 +0900

    Remove unused imports

--- presentation/src/main/java/com/jborchardt/imagefeed/presentation/view/feed/SquareImageView.java
+++ presentation/src/main/java/com/jborchardt/imagefeed/presentation/view/feed/SquareImageView.java
@@ -2,11 +2,10 @@ package com.jborchardt.imagefeed.presentation.view.feed;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 
-
-public class SquareImageView extends ImageView {
+public class SquareImageView extends AppCompatImageView {
     public SquareImageView(final Context context) {
         super(context);
     }
@@ -17,10 +16,6 @@ public class SquareImageView extends ImageView {
 
     public SquareImageView(final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    public SquareImageView(final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
